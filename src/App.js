3f8454f8e21d4a5d53eb3e0f98ee635525f0e7fd
Sonal Rawal <sonu@codeops.tech>
2023-07-17 12:53:37 +0530

    adding meals component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Header from "./components/LayOut/Header";
+import { Fragment } from 'react';
+
+import Header from './components/LayOut/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
-
 
 export default App;
