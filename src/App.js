bb63c90ec679ae6875d36cf496e52b8ececdec9f
Sonal Rawal <sonu@codeops.tech>
2023-07-17 16:15:05 +0530

    adding cart reducer and outputting cart items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment, useState } from 'react';
 
-import Header from './components/Layout/Header';
+import Header from './components/LayOut/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
