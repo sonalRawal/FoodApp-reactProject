00ef0f2366986e84d144d18dd23fede0493df337
Sonal Rawal <sonu@codeops.tech>
2023-07-17 17:06:22 +0530

    fixed some issues

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 import CartContext from '../../../store/cart-context';
@@ -7,12 +8,12 @@ const MealItem = (props) => {
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = (amount) => {
+  const addToCartHandler = amount => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price,
+      price: props.price
     });
   };
 
